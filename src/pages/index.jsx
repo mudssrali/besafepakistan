d88e8576570e840e73496911b4a42c2a7c25c794
mudssrali <mudassaralics007@gmail.com>
2022-08-30 09:16:22 +0500

    add google analytics

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import Script from 'next/script'
 import { CallToAction } from '@/components/CallToAction'
 import { Faqs } from '@/components/Faqs'
 import { Footer } from '@/components/Footer'
@@ -9,9 +9,30 @@ import { WelfareOrganizations } from '@/components/WelfareOrganizations'
 import { ProvincesAtGlance } from '@/components/ProvincesAtGlance'
 import { Fundraisers } from '@/components/Fundraisers'
 
+const GA_ID = 'G-58K2HYYGKW'
+
 export default function Home() {
+  const host = window.location.hostname
+
   return (
     <>
+      {host !== 'localhost' && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GA_ID}');
+        `}
+          </Script>
+        </>
+      )}
       <Head>
         <meta
           key="twitter:title"
