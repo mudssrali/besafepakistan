b8cff71e44bc87bc3c2ba82b3304c7aa49b7d2cc
mudssrali <mudassaralics007@gmail.com>
2022-08-31 19:57:26 +0500

    add donate link and a bit refactor

--- src/components/WelfareOrganizations.jsx
+++ src/components/WelfareOrganizations.jsx
@@ -10,7 +10,6 @@ import saharaImage from '@/images/avatars/sahara.jpg'
 import jdcImage from '@/images/avatars/jdc.jpg'
 import switImage from '@/images/avatars/swit.jpg'
 import handsImage from '@/images/avatars/hands.jpg'
-import byacImage from '@/images/avatars/byac.jpg'
 import imageDawn from '@/images/logos/dawn.png'
 import imageDawateIslami from '@/images/avatars/dawate-islami.jpg'
 import { ArrowRightIcon } from '@heroicons/react/24/solid'
@@ -20,21 +19,23 @@ const oragnizations = [
     {
       content:
         'Akhuwat is supporting brothers & sisters severely affected by floods in Balochistan, Sindh & South Punjab. As an immediate action, Akhuwat intends to present cash assistance worth Rs. 3 to 4 thousand to approximately 10,000 families for food & basic necessities.',
-      author: {
+      organization: {
         name: 'Akhuwat',
         role: 'Interest free Microfinance Orgnization',
         image: akhuwatImage,
-        link: 'https://donate.akhuwat.org.pk/campaign/1365/flood-relief',
+        link: 'https://akhuwat.org.pk',
+        donateLink: 'https://donate.akhuwat.org.pk/campaign/1365/flood-relief',
       },
     },
     {
       content:
         'A salute to the efforts of #AlkhidmatVolunteers who are risking their own lives and going above and beyond to save those in need. Even the darkness of the night, the level of flood water and wreckage can not stop our Volunteers.',
-      author: {
+      organization: {
         name: 'Alkhidmat Foundation Pakistan',
         role: 'Charity Orgnization',
         image: alkhidmatImage,
-        link: 'https://alkhidmat.org/',
+        link: 'https://alkhidmat.org',
+        donateLink: 'https://alkhidmat.org/akf-donations.php',
       },
     },
   ],
@@ -42,21 +43,23 @@ const oragnizations = [
     {
       content:
         'Countless families, women and children are left with nothing but hope, and are in desperate need of chooked food, dry ration packs, tents and medical aid. Donate now and help EDHI Foundation save thousands of precious lives.',
-      author: {
+      organization: {
         name: 'Eidhi Foundation',
         role: 'Donation Platform',
         image: eidhiImage,
-        link: 'https://donate.edhi.org/product/flood-relief-campaign/',
+        link: 'https://edhi.org',
+        donateLink: 'https://donate.edhi.org/product/flood-relief-campaign/',
       },
     },
     {
       content:
         'Once again, I request that you please support us in raising funds by making a contribution towards flood relief in victimized areas. Sahara for Life Trust has already provided assistance in the efforts of resettlement of the deadliest floods of Pakistan (2010–2011) in the past few years.',
-      author: {
+      organization: {
         name: 'Sahara',
         role: 'Charitable Organization',
         image: saharaImage,
-        link: 'https://saharaforlife.org/donations/flood-relief/',
+        link: 'https://saharaforlife.org',
+        donateLink: 'https://saharaforlife.org/donations/flood-relief/',
       },
     },
   ],
@@ -64,30 +67,33 @@ const oragnizations = [
     {
       content:
         'Monsoon rains have led to intense flooding across Pakistan. We ask you all to step forward and be someone’s support.',
-      author: {
+      organization: {
         name: 'Share Rizq',
         role: 'Welfare Orgnization',
         image: rizqImage,
-        link: 'https://sharerizq.com/en/campaigns/191',
+        link: 'https://sharerizq.com',
+        donateLink: 'https://sharerizq.com/en/campaigns/191',
       },
     },
     {
       content:
         'Flood victims are waiting for your help Together with Selani Welfare, join us in helping these Pakistani brothers who are stuck in trouble.',
-      author: {
+      organization: {
         name: 'Saylani Welfare International Trust',
         role: 'Welfare Orgnization',
         image: switImage,
         link: 'https://www.saylaniwelfare.com/',
+        donateLink: 'https://www.saylaniwelfare.com/en/donate',
       },
     },
     {
       content: 'Help the flood victims. Victims are waiting for you',
-      author: {
+      organization: {
         name: 'JDC Foundation',
         role: 'Welfare Organization',
         image: jdcImage,
-        link: 'https://jdcwelfare.org/',
+        link: 'https://jdcwelfare.org',
+        donateLink: 'https://jdcwelfare.org/campaigns/flood-relief-fund/',
       },
     },
   ],
@@ -95,21 +101,12 @@ const oragnizations = [
     {
       content:
         'FGRF is working day and night to help the flood victims. Donation camps established at various places under FGRF for relief in flood affected areas.',
-      author: {
+      organization: {
         name: 'Dawat-e-Islami',
         role: 'Non-political Islamic organization',
         image: imageDawateIslami,
-        link: 'https://twitter.com/MADANIinfo/status/1564636163188613123',
-      },
-    },
-    {
-      content:
-        'BYAC needs yours support for: send in-kind donations to centres, montery support, applify voices of Balochistan people, become a volunteer.',
-      author: {
-        name: 'BYAC',
-        role: 'Welfare Organization',
-        image: byacImage,
-        link: 'https://twitter.com/BYACBalochistan',
+        link: 'https://www.dawateislami.net',
+        donateLink: 'https://www.dawateislami.net/onlinedonation/',
       },
     },
   ],
@@ -117,11 +114,12 @@ const oragnizations = [
     {
       content:
         "Help Pakistan's flood victims by donating to Dawn Relief, We want to extend our efforts to flood-stricken areas across the country. Help us do so by donating generously.",
-      author: {
+      organization: {
         name: 'DAWN Relief',
         role: 'Media Agency',
         image: imageDawn,
-        link: 'https://www.dawn.com/news/1706906',
+        link: '',
+        donateLink: 'https://www.dawn.com/news/1706906',
       },
     },
   ],
@@ -129,11 +127,12 @@ const oragnizations = [
     {
       content:
         'The floods in Sindh, Punjab and Balochistan have taken many lives, homes and livelihoods of people. They need your urgent and help and support.',
-      author: {
+      organization: {
         name: 'HANDS',
         role: 'Non-profit Organization',
         image: handsImage,
-        link: 'https://hands.org.pk/ways-to-donate/',
+        link: 'https://hands.org.pk',
+        donateLink: 'https://hands.org.pk/ways-to-donate/',
       },
     },
   ],
@@ -184,27 +183,29 @@ export function WelfareOrganizations() {
                       </blockquote>
                       <figcaption className="relative mt-6 flex items-center justify-between border-t border-slate-100 pt-6">
                         <div>
-                          <div className="font-display text-base text-slate-900">
-                            {org.author.name}
-                          </div>
-
-                          <div className="mt-1 text-sm text-slate-500">
-                            {org.author.role}
-                          </div>
+                          <p className="text-base font-semibold text-slate-900">
+                            {' '}
+                            {org.organization.name}
+                          </p>
+                          <p className="text-sm text-slate-500">
+                            {org.organization.role}
+                          </p>
                         </div>
-                        <div className="overflow-hidden rounded-full bg-slate-50">
-                          <Image
-                            className="h-14 w-14 object-cover"
-                            src={org.author.image}
-                            alt={org.author.name}
-                            width={56}
-                            height={56}
-                          />
-                        </div>
+                        <Link href={org.organization.link} passHref={true}>
+                          <div className="overflow-hidden rounded-full bg-slate-50">
+                            <Image
+                              className="h-14 w-14 object-cover"
+                              src={org.organization.image}
+                              alt={org.organization.name}
+                              width={56}
+                              height={56}
+                            />
+                          </div>
+                        </Link>
                       </figcaption>
                       <Link
-                        className="mt-6 inline-flex text-sm items-center text-blue-500 group"
-                        href={org.author.link}
+                        className="group mt-2 inline-flex items-center text-sm text-blue-500"
+                        href={org.organization.donateLink}
                         passHref={true}
                       >
                         <span>DONATE NOW</span>
